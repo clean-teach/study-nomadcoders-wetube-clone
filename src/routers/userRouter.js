eb27b265fd6d5def3eb8a9f952c377474dafc623
clean-teach <82921898+clean-teach@users.noreply.github.com>
2022-06-18 03:58:49 +0900

    Planing Route
    1. 본격적인 Route 구조를 만들기 앞서서, URL 계획부터 짠다.
    2. URL 계획은 사용자 입장에서 생각해본다.
    3. URL 계획
        - / → 메인 페이지
        - /join → 회원가입 페이지
        - /login → 로그인 페이지
        - /search → 검색 페이지
        - **/users/**
            - **/users/**:id → See User
            - **/users/**logout → Logout
            - **/users/**edit → Edit My Profile
            - **/users/**delete → Delete My Profile
        - **/videos/**
            - **/videos/**:id → See Video
            - **/videos/**:id/edit → 영상 수정 페이지
            - **/videos/**:id/delete → 영상 삭제 페이지
            - **/videos/**upload → Upload Video

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,9 +1,11 @@
 import express from 'express'
-import { edit, remove } from '../controllers/userControllers';
+import { see, edit, remove, logout } from '../controllers/userControllers';
 
 const userRouter = express.Router();
 
+userRouter.get(':id', see);
 userRouter.get('/edit', edit);
 userRouter.get('/remove', remove);
+userRouter.get('/logout', logout);
 
 export default userRouter;
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,9 +1,11 @@
 import express from 'express'
-import { edit, watch } from '../controllers/videoControllers';
+import { edit, remove, see, upload } from '../controllers/videoControllers';
 
 const videoRouter = express.Router();
 
-videoRouter.get('/watch', watch);
-videoRouter.get('/edit', edit);
+videoRouter.get('/upload', upload);
+videoRouter.get('/:id', see);
+videoRouter.get('/:id/edit', edit);
+videoRouter.get('/:id/delete', remove);
 
 export default videoRouter;
