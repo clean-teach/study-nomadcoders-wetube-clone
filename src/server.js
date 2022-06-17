2cf9fdf875068d5de2b25171397de0441edcde4b
clean-teach <82921898+clean-teach@users.noreply.github.com>
2022-06-17 23:04:39 +0900

    router first commit

--- src/server.js
+++ src/server.js
@@ -1,6 +1,28 @@
 import express from 'express'
-// const express = require('express');
-// const app = express();
+import morgan from 'morgan';
 
-const txt = 'hello world';
-console.log(txt);
+const PORT = 4000;
+const app = express();
+const logger = morgan('dev');
+
+app.use(logger);
+
+const globalRouter = express.Router();
+const handleHome = (req, res) => res.send('Home');
+globalRouter.get('/', handleHome);
+const userRouter = express.Router();
+const handleEditUser = (req, res) => res.send('Edit User');
+userRouter.get('/edit', handleEditUser);
+const videoRouter = express.Router();
+const handleWatchVideo = (req, res) => res.send('Watch Video');
+videoRouter.get('/watch', handleWatchVideo);
+
+app.use('/', globalRouter);
+app.use('/users', userRouter);
+app.use('/videos', videoRouter);
+
+const handleListen = () => {
+    console.log(`server listening on port http://localhost:${PORT}`);
+};
+
+app.listen(PORT, handleListen);
