6157a763de7334400956830c6518eda6ce9af33e
clean-teach <82921898+clean-teach@users.noreply.github.com>
2022-06-21 22:59:39 +0900

    Returning HTML
    기본적으로 따옴표 안에 HTML 문자열을 직접 기입해주는 방법이 있다.
    - 코드가 너무 길고 가독성이 좋지 않다.
    - 같은 내용을 복사 붙여넣기 해야 하는 등 너무 비효율적이다.

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,5 +1,7 @@
 export const search = (req, res) => res.send('Search Video');
-export const trending = (req, res) => res.send('Home Page Video');
+export const trending = (req, res) => {
+    return res.send(`<!DOCTYPE html><html lang='ko'><head><title>Wetube</title></head><body><h1>Home</h1><footer>&copy; 2021 Wetube</footer></body></html>`)
+};
 export const upload = (req, res) => res.send('Upload Video');
 export const see = (req, res) => {
     return res.send(`Watch Video #${req.params.id}`);
