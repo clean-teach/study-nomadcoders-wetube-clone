e791fd4686cc9b5aefbee43dab26e04530bd2d70
clean-teach <82921898+clean-teach@users.noreply.github.com>
2022-06-22 02:41:56 +0900

    partials
    - include 추가

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,12 +1,7 @@
 export const search = (req, res) => res.send('Search Video');
-export const trending = (req, res) => {
-    // return res.send(`<!DOCTYPE html><html lang='ko'><head><title>Wetube</title></head><body><h1>Home</h1><footer>&copy; 2021 Wetube</footer></body></html>`)
-    return res.render('home')
-};
+export const trending = (req, res) => res.render('home');
 export const upload = (req, res) => res.send('Upload Video');
-export const see = (req, res) => {
-    return res.send(`Watch Video #${req.params.id}`);
-};
+export const see = (req, res) => res.render('watch');
 export const edit = (req, res) => {
     return res.send(`Edit Video #${req.params.id}`);
 };
