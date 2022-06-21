78c43d0603ac8ac87562235ba7825f9be4e4a722
clean-teach <82921898+clean-teach@users.noreply.github.com>
2022-06-22 03:09:58 +0900

    Extending Templates
    - Inheritance(상속) : base Template 를 만들고 거기에서 각 페이지들을 확장하는 개념
    - block : base Template 의 일부분을 각 페이지에서 작성 할 수 있게 하는 창구

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -2,9 +2,7 @@ export const search = (req, res) => res.send('Search Video');
 export const trending = (req, res) => res.render('home');
 export const upload = (req, res) => res.send('Upload Video');
 export const see = (req, res) => res.render('watch');
-export const edit = (req, res) => {
-    return res.send(`Edit Video #${req.params.id}`);
-};
+export const edit = (req, res) => res.render('edit');;
 export const remove = (req, res) => {
     return res.send(`Delete Video #${req.params.id}`);
 };
