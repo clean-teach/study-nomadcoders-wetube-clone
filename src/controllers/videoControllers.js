290b15c302e957451870b52c51294bb5475d5ef9
clean-teach <82921898+clean-teach@users.noreply.github.com>
2022-06-24 00:35:28 +0900

    Iteration - each
    - template에서 반복문을 사용하기 위해서는 기본적으로 배열(array)이 필요
    - 배열 내 아이템 (위 예시에서는 “potato”)의 이름은 임의로 지정 가능
        - 하지만, 통상적으로 item의 이름은 배열(**복수형**) 이름의 **단수형**을 사용한다.
            - 예 :
                - array : potatos
                - item : potato
    - 배열의 이름은 선언된 이름과 일치 하여야 함.
        - 일치하지 않을 경우, error 발생
    - pug 에서는 배열이 비어 있을 경우를 대비해서 반복문에도 `else`문을 제공한다. (자바스크립트에는 없는 pug만의 문법)

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -3,7 +3,10 @@ const fakeUser = {
     loggedIn: false
 };
 
-export const trending = (req, res) => res.render('home', {pageTitle: 'Home', lovesTarget: 'tomato', fakeUser});
+export const trending = (req, res) => {
+    const videos = [1, 2, 3, 4, 5, 6, 7, 8]
+    return res.render('home', {pageTitle: 'Home', lovesTarget: 'tomato', fakeUser, videos})
+};
 export const see = (req, res) => res.render('watch', {pageTitle: 'Watch'});
 export const edit = (req, res) => res.render('edit', {pageTitle: 'Edit'});
 export const search = (req, res) => res.send('Search Video');
