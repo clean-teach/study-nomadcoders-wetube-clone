470c657c91bbafcb47516a70e63374eb723247a0
clean-teach <82921898+clean-teach@users.noreply.github.com>
2022-06-27 23:37:32 +0900

    Array Database part One
    연습용 가짜 데이터 베이스로 링크 연결

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -1,36 +1,44 @@
 const fakeUser = {
     username: 'nico',
-    loggedIn: false
+    loggedIn: true
 };
+let videos = [
+    {
+        title: 'First video', 
+        rating: 2,
+        comments: 5,
+        createAt: '2 minutes ago',
+        views: 59,
+        id: 1
+    },
+    {
+        title: 'Second video', 
+        rating: 2,
+        comments: 5,
+        createAt: '2 minutes ago',
+        views: 59,
+        id: 2
+    },
+    {
+        title: 'Third video', 
+        rating: 2,
+        comments: 5,
+        createAt: '2 minutes ago',
+        views: 59,
+        id: 3
+    }
+];
 
 export const trending = (req, res) => {
-    const videos = [
-        {
-            title: 'First video', 
-            rating: 2,
-            comments: 5,
-            createAt: '2 minutes ago',
-            views: 59
-        },
-        {
-            title: 'Second video', 
-            rating: 2,
-            comments: 5,
-            createAt: '2 minutes ago',
-            views: 59
-        },
-        {
-            title: 'Third video', 
-            rating: 2,
-            comments: 5,
-            createAt: '2 minutes ago',
-            views: 59
-        }
-    ]
     return res.render('home', {pageTitle: 'Home', lovesTarget: 'tomato', fakeUser, videos})
 };
-export const see = (req, res) => res.render('watch', {pageTitle: 'Watch'});
-export const edit = (req, res) => res.render('edit', {pageTitle: 'Edit'});
+export const see = (req, res) => {
+    // const id = req.params.id;
+    const {id} = req.params;
+    const video = videos[id - 1];
+    return res.render('watch', {pageTitle: `Watching ${video.title}`, fakeUser});
+};
+export const edit = (req, res) => res.render('edit', {pageTitle: 'Edit', fakeUser});
 export const search = (req, res) => res.send('Search Video');
 export const upload = (req, res) => res.send('Upload Video');
 export const remove = (req, res) => {
