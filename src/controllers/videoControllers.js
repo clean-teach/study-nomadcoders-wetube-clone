d8a3ee3fdb62373ff230f338c688a475c1a1be7a
clean-teach <82921898+clean-teach@users.noreply.github.com>
2022-07-02 04:05:46 +0900

    Exceptions and Validation
    1. Model에서 정의한 Data Type과는 다른 Data를 입력하거나, Data가 누락되어 입력되는 경우
        - `**required**` 필수 입력 처리 할 수 있다. ([참고](https://mongoosejs.com/docs/api.html#schematype_SchemaType-isRequired))
            - `{type: String, **required: true**}`
    
    → Mongoose는 DB 전송을 중단하고 error를 반환한다.
    
    - **Data Type**은 구체적일수록 좋다.
        - 인간의 실수를 방지 할 수 있다.
    1. `try{}catch(error){}` 문으로 error 상황에 대응해 준다.
        1. error 가 발생했을때에도, 화면이 그냥 멈춰있지 않고 다시 입력을 시도할 수 있는 화면을 rendering 해준다.
        2. error 내용을 안내해준다.
            1. error의 내용은 아주 많기 때문에, 그중 `error._messge` 부분을 rendering 해준다.
            2. 당연히 해당 부분을 View(.pug)파일에도 추가 해주어야 한다.
    2. Mongoose는 Model에 Data Type 뿐만 아니라, **Default Data**도 설정 할 수 있다. ([참고](https://mongoosejs.com/docs/api.html#schematype_SchemaType-default))
        - `{type: String, required: true, default: Data.now}`
        - 포인트는 default 에 들어가는 method의 경우 괄호() 를 적지 않는 것이다.
            - Type을 정의할때 호출을 하는 것이 아니기 때문

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -4,33 +4,15 @@ const fakeUser = {
     username: 'nico',
     loggedIn: true
 };
-
-// Callback 방식
-// export const home = (req, res) => {
-//     console.log('Start');
-//     Video.find({}, (error, videos) => {
-//         console.log('Error : ', error);
-//         console.log('Videos : ', videos);
-//         return res.render('home', {pageTitle: 'Home', lovesTarget: 'tomato', fakeUser, videos});
-//     });
-//     console.log('finish');
-// };
-
-// Promise 방식
 export const home = async (req, res) => {
-    console.log('Start');
     try{
         const videos = await Video.find({});
-        console.log('Videos : ', videos);
         return res.render('home', {pageTitle: 'Home', lovesTarget: 'tomato', fakeUser, videos});
     }catch(error){
         console.log('Error : ', {error});
     }
-    console.log('finish');
 };
-
 export const watch = (req, res) => {
-    // const id = req.params.id;
     const {id} = req.params;
     return res.render('watch', {pageTitle: `Watching : `, fakeUser});
 };
@@ -48,28 +30,16 @@ export const getUpload = (req, res) => {
 };
 export const postUpload = async (req, res) => {
     const {title, description, hashtagsd} = req.body;
-    // const video = new Video({
-    //     title,
-    //     description,
-    //     createAt: Date.now(),
-    //     hashtags: hashtagsd.split(',').map(hashtag => `#${hashtag}`),
-    //     meta: {
-    //         views: 0,
-    //         rating:0,
-    //     },
-    // });
-    // await video.save();
-    await Video.create({
-        title,
-        description,
-        createAt: Date.now(),
-        hashtags: hashtagsd.split(',').map(hashtag => `#${hashtag}`),
-        meta: {
-            views: 0,
-            rating:0,
-        },
-    });
-    return res.redirect(`/`);
+    try{
+        await Video.create({
+            title,
+            description,
+            hashtags: hashtagsd.split(',').map(hashtag => `#${hashtag}`),
+        });
+        return res.redirect(`/`);
+    }catch(error){
+        return res.render('upload', {pageTitle: `Upload Video`, fakeUser, errorMessage: error._message});
+    }
 };
 // export const search = (req, res) => res.send('Search Video');
 // export const remove = (req, res) => {
