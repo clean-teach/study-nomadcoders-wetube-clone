8e986295bcf094d4f699e05dadcb720a156247f4
clean-teach <82921898+clean-teach@users.noreply.github.com>
2022-07-08 02:08:51 +0900

    5.19 Video Detail
    - MongoDB에서 자동으로 부여해주는 ID 값에 맞춰서 Route를 설정 해 줄 필요가 있다.
    
        → 방법은 두가지가 있다.
    
        1. `:id` router의 순서를 가장 아래에 입력
            - 같은 경로 내에서 :id 값을 사용하는 router가 위에 있다면, 일반 디렉토리로 :id로 취급 받을 염려가 있다.
        2. route에서 **정규식** 사용하기
            1. MongoDB에서 부여하는 ID값의 규칙을 조회
    
                [ObjectID() - MongoDB Node.JS Driver 1.4.9 documentation](https://mongodb.github.io/node-mongodb-native/api-bson-generated/objectid.html#)
    
                - [hexadecimal](https://ko.wikipedia.org/wiki/%EC%8B%AD%EC%9C%A1%EC%A7%84%EB%B2%95) (16진법)
            2. ID값의 규칙에 맞춰서 정규식 제작 ([정규식 테스터](https://www.notion.so/35dfcca4798f46ccb21d2532b36740fd))
                - `/[0-9a-f]{24}/g`
                    - 숫자 0부터 9와 알파벳 a부터 f를 사용하는 24자리 String을 나타내는 정규식
    - `[Model.findOne()](https://mongoosejs.com/docs/api/model.html#model_Model.findOne)` : 모든 condition을 사용해서 DB에서 데이터를 불러온다.
    - `[Model.findById()](https://mongoosejs.com/docs/api/model.html#model_Model.findById)` : id로 조회해서 DB에서 데이터를 불러온다.

--- src/controllers/videoControllers.js
+++ src/controllers/videoControllers.js
@@ -12,9 +12,10 @@ export const home = async (req, res) => {
         console.log('Error : ', {error});
     }
 };
-export const watch = (req, res) => {
+export const watch = async (req, res) => {
     const {id} = req.params;
-    return res.render('watch', {pageTitle: `Watching : `, fakeUser});
+    const video = await Video.findById(id);
+    return res.render('watch', {pageTitle: video.title, video, fakeUser});
 };
 export const getEdit = (req, res) => {
     const {id} = req.params;
