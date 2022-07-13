fdfc2dc6e90010b9e080b88c93ac52d2a959c98b
clean-teach <82921898+clean-teach@users.noreply.github.com>
2022-07-14 06:33:05 +0900

    Creating Account
    1. **Model** 만들기
        1. import mongoose
        2. schema 생성
            - User 데이터의 **형태**를 만든다. ([참고](https://mongoosejs.com/docs/schematypes.html#schematype-options))
            - `[{unique: true}](https://www.mongodb.com/docs/manual/core/index-unique/)` : 고유 정보. 중복 값을 저장하지 않는다.
        3. schema를 참조하는 Model 생성
        4. export model
        5. [init.js](https://www.notion.so/35dfcca4798f46ccb21d2532b36740fd) 파일에 Model import 해주기
    2. **View** (Template) 만들기
        - `<form>` template 을 만든다면, `<input>`태그에 `name` 속성을 꼭 추가한다.
            - `name` 속성이 없으면, back-end에서 `<input>`을 다룰 수 없다.
    3. **Controller** 만들기
        - get 요청의 경우, Template를 rendering 해줄 수 있다.
        - post 요청의 경우 `Model.create({})` 등의 메소드를 사용 할 수 있다.
            - import Model 필요
            - password 는 그냥 서버에 업로드 하면 보안상 심각하게 위험하다.
    
                → **password hashing :**
    
                - [해싱 체험해보기 →](https://emn178.github.io/online-tools/sha256.html)
                - 일방향 함수.
                    - 입력값이 다른 형태의 출력값으로 변환 되지만, 절대 되돌릴 수 없다.  (출력값으로 입력값을 알아낼 수 없다.)
                    - 같은 입력값을는 항상 같은 출력값이 나온다. (결정적 함수, deterministic function)
                - [bcrypt](https://www.npmjs.com/package/bcrypt)
                    1. `npm i bcrypt` : npm 설치
                    2. 사용
    
                        ```jsx
                        bcrypt.hash(myPlaintextPassword, saltRounds, function(err, hash) {
                            // Store hash in your password DB.
                        });
                        ```
    
                        - `saltRounds` : hash 된 결과 값을 특정 숫자만큼 반복해서 여러번 hashing 하는 것.
                        - Model.js
    
                            ```jsx
                            import bcrypt from 'bcrypt';
    
                            Schem.pre('save', async function(){
                            	this.password = await bcrypt.hash(this.password, 5); // async await를 쓰고 있기 때문에, callback function은 필요 없다.
                            });
                            ```
    
                            1. `import bcrypt`
                            2. `schema.pre()`
                                - [middle ware 로 사용](https://www.notion.so/35dfcca4798f46ccb21d2532b36740fd)
                                - `this.password` 는 유저가 입력한 password를 말한다.
                            3. 비동기 (`async await`) 로 사용
    4. **Router** 로 URL과 Controller 연결 해주기
        - `globalRouter` → `rootRouter` 명칭 변경
            - server.js 파일에서도 해당 파일 명칭 수정

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,4 +1,13 @@
-export const join = (req, res) => res.send('Join');
+import User from '../models/User'
+
+export const getJoin = (req, res) => res.render('join', {pageTitle: 'Join'});
+export const postJoin = async (req, res) => {
+    const {name, email, username, password, location} = req.body;
+    await User.create({
+        name, email, username, password, location
+    });
+    res.redirect('/login');
+};
 export const login = (req, res) => res.send('login');
 export const logout = (req, res) => res.send('logout');
 export const see = (req, res) => {
