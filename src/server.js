836e764197803deff6f94fcba5e805bd57aac54a
clean-teach <82921898+clean-teach@users.noreply.github.com>
2022-07-16 02:14:01 +0900

    Sessions and Cookies 등
    - 브라우저에게 로그인 정보 기억하게 하기
    - **세션 (Session)** :
        - 백엔드와 브라우저 간에 어떤 활동을 했는지 기억하는 것
        - 그렇기 위해서는 브라우저와 백엔드가 서로에 대한 정보를 갖게 하여야 한다.
        - 백엔드에서 HTTP 요청을 보내고 나면, 서버와 백엔드의 연결은 끊어지기 때문이다. ⇒ Stateless (무상태)
    
            → 그래서 유저가 백엔드에 무언가를 요청할 때마다, 누가 요청하는지 특정 텍스트를 같이 보낸다.
    
        - **[express-session](https://www.npmjs.com/package/express-session)** :
            - express 에서 session을 처리 할 수 있게 해주는 미들웨어
            - 사용법 :
                1. `npm i express-session`
                2. server.js 에서 `import express-session`
                3. server.js 에서 `router` 전에 `app.use(session({}))`
                    - `secret` :
                        - 필수옵션
                        - 세션 ID 쿠키에 서명하는 데 사용되는 비밀.
                        - 이것은 단일 비밀에 대한 문자열이거나 여러 비밀의 배열일 수 있습니다
                    - `resave` :
                        - 요청하는 동안 세션이 수정되지 않은 경우에도 세션이 세션 저장소에 다시 저장되도록 합니다
                    - `saveUninitialized` :
                        - "초기화되지 않은" 세션을 저장소에 강제로 저장. 세션은 새 것이지만 수정되지 않은 경우 초기화되지 않습니다
    
                ⇒ 사이트에 접속하는 모두를 기억한다. 로그인 하지 않았더라도..
    
            - 요청이 있을때마다 express가 자동으로 브라우저를 위한 세션 id를 만들고 브라우저에도 그 세션 id를 보낸다.
            - 브라우저는 세션 id를 쿠키에 저장하는데, 브라우저가 세션 id를 보낸 도메인 하의 모든 URL에 요청을 보낼 때마다 세션 id를 요청과 함께 보낸다.
            - 브라우저별로 다른 세션 id를 받기 때문에 다른 쿠키로 인식한다.
                - 세션 id 마다 개별적인 일종의 DB인 쿠키를 갖는데, 같은 요청에도 각각의 세션 id 안에 있는 DB 데이터는 따로 관리 된다.
            - 서버가 재시작 할 때마다 세션이 사라지는데, express 가 session을 메모리에 저장하고 있기 때문이다.
    
                → 백엔드가 session 을 잊지 않도록, MongoDB에 연결

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express, { urlencoded } from 'express'
 import morgan from 'morgan';
+import session from 'express-session';
 import rootRouter from './routers/rootRouter';
 import userRouter from './routers/userRouter';
 import videoRouter from './routers/videoRouter';
@@ -12,6 +13,24 @@ app.set('views', process.cwd() + '/src/views');
 
 app.use(logger);
 app.use(urlencoded({extended: true}));
+
+app.use(session({
+    secret: 'Hello',
+    resave: true,
+    saveUninitialized: true,
+}));
+
+app.use((req, res, next) => {
+    req.sessionStore.all((error, session) => {
+        console.log(session);
+        next();
+    });
+});
+
+app.get('/add-one', (req, res, next) => {
+    req.session.potato += 1;
+    return res.send(`${req.session.id} <br><br> ${req.session.potato}`);
+});
 
 app.use('/', rootRouter);
 app.use('/users', userRouter);
