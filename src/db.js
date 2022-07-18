edd253dbc56fe80d1ec15918f056ba53cc20102e
clean-teach <82921898+clean-teach@users.noreply.github.com>
2022-07-19 02:54:52 +0900

    #7.15 Environment Variables
    [dotenv](https://www.npmjs.com/package/dotenv)
    
    - .env file을 읽고 .env file의 변수들을 `process.env` 안에 넣는 package
    - 매우 유명한 패키지 이다.
    - JavaScript, python 등 여러 언어로 구혀뇌어 있다.
    - 사용법 :
        1. `npm i dotenv`
        2. application에서 가장 먼저 가장 먼저 `require('dotenv').config();` 를 사용한다.
            - 최대한 빨리 env 를 load 하여야 하기 때문
            - require 방식으로 사용하려면, env를 사용하는 모든 파일에서 dotenv를 불러와야 한다.
    
                 → import 방식으로 사용한다면, application 내에서 한번만 불러올 수 있다.
    
                - `import 'dotenv/config';`
                - init.js file이 이번 project application에서 가장 먼저 실행되는 root file 이므로, init.js file의 최상단에서 사용해준다.

--- src/db.js
+++ src/db.js
@@ -1,10 +1,6 @@
 import mongoose from 'mongoose';
 
-mongoose.connect(process.env.DB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-});
+mongoose.connect(process.env.DB_URL, {});
 
 const db = mongoose.connection;
 
