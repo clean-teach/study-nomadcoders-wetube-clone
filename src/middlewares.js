827ed8d6f3c2cbc61542f90b4dbf2eb199632ca6
clean-teach <82921898+clean-teach@users.noreply.github.com>
2022-07-21 02:00:33 +0900

    #8.1 Protector and Public Middlewares
    - **Login 하지 않는 사용자**가 Profile 페이지를 방문하지 못하게 URL을 보호하여야 한다.
    
        ⇒ **Protector** Middle wares
    
    - 반대로, **Login 한 사용자**가 방문하지 못하는 페이지도 보호 하여야 한다.
    
        ⇒ **Public Only** Middle wares
    
    ## Protector Middle wares
    
    1. `Middleware.js` file에 `protectorMiddleware(req, res, next)` 를 만든다.
        - 사용자가 로그인 되어 있는지 확인
            - 로그인 되어 있지 않으면
    
                → 로그인 페이지로 redirect
    
            - 로그인 되어 있으면
    
                → request 를 계속하게 한다.
    
    ## Public Only Middle wares
    
    1. `Middleware.js` file에 `publicOnlyMiddleware(req, res, next)` 를 만든다.
        - 사용자가 로그인 되어 있는지 확인
            - 로그인 되어 있으면
    
                → request 를 계속하게 한다.
    
            - 로그인 되어 있지 않으면
    
                → 로그인 redirect
    
    ## Router 에 Middle ware 적용
    
    1. 필요한 Route에 Middle ware를 적용한다.
        - Login 상태에서만 접근 할 Route :
            - `'profile'`
            - `'logout'`
        - Logout 상태에서만 접근 할 페이지 :
            - `'github/start'`
            - `'github/finish'`
            - `'join'`
            - `'login'`
        - GET과 POST 요청이 둘 다 존재하는 Router의 경우 `.all()` Method를 사용하면, 모든 Http 요청에 같은 Middle ware를 적용 할 수 있다.
            - `[.all()](https://www.geeksforgeeks.org/express-js-app-all-function/)` : express 가 알아서 모든 HTTP 요청(Get, Post, Put, Delete 등)에 controller를 호출 할 수 있게 해주는 Method

--- src/middlewares.js
+++ src/middlewares.js
@@ -4,3 +4,17 @@ export const localsMiddleware = (req, res, next) => {
     res.locals.siteName = 'Wetube';
     next();
 };
+export const protectorMiddleware = (req, res, next) => {
+    if(req.session.loggedIn){
+        return next();
+    }else{
+        return res.redirect('/login');
+    }
+};
+export const publicOnlyMiddleware = (req, res, next) => {
+    if(!req.session.loggedIn){
+        return next();
+    }else{
+        return res.redirect('/');
+    }
+};
