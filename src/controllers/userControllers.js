5b93efa71fa316583673f1cd233b8c46880bbd82
clean-teach <82921898+clean-teach@users.noreply.github.com>
2022-07-21 01:24:57 +0900

    #8.0 Edit Profile GET
    1. Edit profile 은 GET과 POST 요청이 필요하다.
    2. Get과 Post 의 Controller와 Router를 만든다.
    3. Template에 `/edit` URL에 접근 할 수 있는 link를 만든다.
        1. Header의 Navigation에 만든다. (Login 되어 있는 경우에만)
    4. Form이 들어갈 Template을 만든다.
        1. form은 POST 요청을 보낸다.
        2. form은 현재 페이지와 같은 URL로 요청을 보낼 것이기 때문에 action 속성은 작성하지 않는다.
        3. Model을 참고하여, 수정 할 항목을 정하고 input을 만든다.
            - password 수정 페이지는 나중에 따로 만들 것이다.
                - 왜냐하면 비밀번호가 없는 사용자도 존재하기 때문 (git hub login 참조)
        4. 수정 페이지 이기 때문에 input에는 기존에 입력된 Data가 input의 value에 기본값으로 들어가 있어야 한다.
            - 모든 Template에 자동적으로 import 되는 **locals Middle ware** 를 사용한다.
    
    ❗Login 하지 않은 누군가가 Login하여야만 하는 페이지에 접근할 경우 문제가 있다.
    
    - Login 하지 않고 `locals.loggedInUser` 를 불러올 경우 error 가 발생
    
        ⇒ **locals Middle ware** 에서 `locals.loggedInUser` 의 값을 비어있는 Object를 or 연산자로 추가 한다.
    
        - `|| {}`
    - Login 하지 않은 사용자는 접근 할 수 없어야 한다.
    
        ⇒ Route 를 보호 한다.

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -106,7 +106,12 @@ export const logout = (req, res) => {
     req.session.destroy();
     return res.redirect('/');
 };
+export const getEdit = (req, res) => {
+    return res.render('edit-profile', {pageTitle: 'Edit Profile'});
+};
+export const postEdit = (req, res) => {
+    return res.render('edit-profile');
+};
 export const see = (req, res) => {
     return res.send(`See User #${req.params.id}`)
 };
-export const edit = (req, res) => res.send('Edit User');
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,12 +1,12 @@
 import express from 'express'
-import { see, edit, logout, startGithubLogin, finishGithubLogin } from '../controllers/userControllers';
+import { see, logout, startGithubLogin, finishGithubLogin, getEdit, postEdit } from '../controllers/userControllers';
 
 const userRouter = express.Router();
 
 userRouter.get('/logout', logout);
-userRouter.get('/edit', edit);
 userRouter.get('/github/start', startGithubLogin);
 userRouter.get('/github/finish', finishGithubLogin);
+userRouter.route('/edit').get(getEdit).post(postEdit);
 userRouter.get('/:id', see);
 
 export default userRouter;
