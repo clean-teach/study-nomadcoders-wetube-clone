64852e4a09b67004903805fcd813604a39d0107f
clean-teach <82921898+clean-teach@users.noreply.github.com>
2022-07-23 02:27:17 +0900

    #11.2 Mute and Unmute
    - Video 의 상태에 따라서 click event handler 안에서 조건을 분류 할 수 있다.
    
        [HTMLMediaElement - Web API | MDN](https://developer.mozilla.org/ko/docs/Web/API/HTMLMediaElement)
    
    - 단순한 문자 대체의 경우, 삼항 연산자를 사용하면 조금 더 세련된 분류도 가능하다.
    - 음소거 버튼은 볼륨 버튼과 연계 되어야 한다.

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -2,7 +2,7 @@ const video = document.querySelector('video');
 const playBtn = document.getElementById('play');
 const muteBtn = document.getElementById('mute');
 const time = document.getElementById('time');
-const volume = document.getElementById('volume');
+const volumeRange = document.getElementById('volume');
 
 const handlePlayClick = (e) => {
     if(video.paused){
@@ -10,12 +10,17 @@ const handlePlayClick = (e) => {
     }else{
         video.pause();
     }
+    playBtn.innerText = video.paused ? 'play' : 'Pause';
 };
-const handleMute = (e) => {};
-const handlePlay = (e) => {playBtn.innerText = 'Pause';};
-const handlePause = (e) => {playBtn.innerText = 'Play';};
+const handleMute = (e) => {
+    if(video.muted){
+        video.muted = false;
+    }else{
+        video.muted = true;
+    }
+    muteBtn.innerText = video.muted ? 'Unmuted' : 'Muted';
+    volumeRange.value = video.muted ? 0 : .5;
+};
 
 playBtn.addEventListener('click', handlePlayClick);
 muteBtn.addEventListener('click', handleMute);
-video.addEventListener('play', handlePlay);
-video.addEventListener('pause', handlePause);
