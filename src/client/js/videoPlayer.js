ecf8bb804446c8cf6d7e8e3e3cdec2f71db9d71f
clean-teach <82921898+clean-teach@users.noreply.github.com>
2022-07-23 03:02:27 +0900

    #11.3 Volume
    - [range](https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/range)의 event 는 크게 두 종류 이다.
        - `change` : 변화 완료 후 감지
        - `input` : 실시간 변화 감지
    - `event` 인수명을 `e` 로 줄여서 작성하는 것은 좋지 않다.
        - `event.target.value` 로 range 값에 접근 할 수 있다.
    - `video.volume` 으로 비디오의 볼륨에 접근 할 수 있다.
    - 음소거 버튼을 누르기 전 볼륨 값을 기억하기 위하여 `let` 전역 변수를 사용 할 수 있다.
    - [ ]  `localStorage` 를 사용하면, 볼륨의 상태 등도 저장 할 수 있다.
    - [ ]  반복되는 코드는 좋지 않으니 정리 할 수 있다.

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -4,7 +4,10 @@ const muteBtn = document.getElementById('mute');
 const time = document.getElementById('time');
 const volumeRange = document.getElementById('volume');
 
-const handlePlayClick = (e) => {
+let volumeValue = .5;
+video.volume = volumeValue;
+
+const handlePlayClick = (event) => {
     if(video.paused){
         video.play();
     }else{
@@ -12,15 +15,24 @@ const handlePlayClick = (e) => {
     }
     playBtn.innerText = video.paused ? 'play' : 'Pause';
 };
-const handleMute = (e) => {
+const handleMute = (event) => {
     if(video.muted){
         video.muted = false;
     }else{
         video.muted = true;
     }
-    muteBtn.innerText = video.muted ? 'Unmuted' : 'Muted';
-    volumeRange.value = video.muted ? 0 : .5;
+    muteBtn.innerText = video.muted ? 'Unmute' : 'Mute';
+    volumeRange.value = video.muted ? 0 : volumeValue;
+};
+const handleVolumeChange = (event) => {
+    const {
+        target: { value }
+    } = event;
+    muteBtn.innerText = value >= 0.1 ? 'Mute' : 'Unmute';
+    volumeValue = value;
+    video.volume = value;
 };
 
 playBtn.addEventListener('click', handlePlayClick);
 muteBtn.addEventListener('click', handleMute);
+volumeRange.addEventListener('input', handleVolumeChange);
