7d4f3b62fecbc60df9dcf1a48200ae2946be4da1
clean-teach <82921898+clean-teach@users.noreply.github.com>
2022-07-23 02:10:16 +0900

    #11.1 Play Pause
    1. `pug` 에서 Video Player HTML을 만든다.
    2. 필요하다면 `SCSS`에서 Style을 다듬는다.
    3. `videoPlayer.js` 작성
        1. `DOM` 요소를 불러온다
        2. Event 를 Listen 한다.
        3. handler 작성
            - button click event 와 video 관련 event를 구분한다.
                - button click event : video를 조작
                - **[video event](https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement/play_event)** : video 상태에 따른 button를 조작

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,1 +1,21 @@
-console.log('video player');
+const video = document.querySelector('video');
+const playBtn = document.getElementById('play');
+const muteBtn = document.getElementById('mute');
+const time = document.getElementById('time');
+const volume = document.getElementById('volume');
+
+const handlePlayClick = (e) => {
+    if(video.paused){
+        video.play();
+    }else{
+        video.pause();
+    }
+};
+const handleMute = (e) => {};
+const handlePlay = (e) => {playBtn.innerText = 'Pause';};
+const handlePause = (e) => {playBtn.innerText = 'Play';};
+
+playBtn.addEventListener('click', handlePlayClick);
+muteBtn.addEventListener('click', handleMute);
+video.addEventListener('play', handlePlay);
+video.addEventListener('pause', handlePause);
