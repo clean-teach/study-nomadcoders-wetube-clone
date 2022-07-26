45874cbc57ff3876c1f585acae357078539c3c24
clean-teach <82921898+clean-teach@users.noreply.github.com>
2022-07-27 02:55:13 +0900

    #11.9 Controls Events
    ## 목표
    
    - mouse 가 video 위로 올라 갔을 때, control bar를 보여준다.
    - mouse 가 video 위에서 움직일 때, control bar를 보여준다.
    - mouse 가 video 위에서 벗어났을 때, control bar를 몇 초 후 숨긴다.
    - mouse 가 video 위에서 멈췄을 때, control bar를 몇 초 후 숨긴다.
    
    ### 학습 point
    
    - JavaScript event
    - `setTimeout(function, time)` : 지정된 시간 후 함수 실행
    - `clearTimeout(setTimeoutId)` : time out 취소
    
    ## 구현
    
    [이벤트 참조 | MDN](https://developer.mozilla.org/ko/docs/Web/Events)
    
    - [`mousemove`](https://developer.mozilla.org/en-US/docs/Web/API/Element/mousemove_event) : 포인팅 장치가 엘리먼트 위에서 이동했을 때
    - [`mouseleave`](https://developer.mozilla.org/en-US/docs/Web/API/Element/mouseleave_event) : 포인팅 장치가 리스너가 등록된 엘리먼트 밖으로 이동했을 때.
    - `setTimeout()` 은 고유의 id를 반환한다.
        - 이 id를 전역 변수에 담으면, 다른 이벤트 함수에서 `clearTimeout()` 에 사용 할 수 있다.
    - 마우스가 멈추는 것을 감자하는 이벤트는 없으므로, `setTimeout()`과 `clearTimeout()` 으로 이를 구현해 본다.
        1. [`mousemove`](https://developer.mozilla.org/en-US/docs/Web/API/Element/mousemove_event) event handler 안에서 hide 함수를 `setTimeout()` 으로 실행해준다.
        2. 그리고 바로 `clearTimeout()` 으로 위의 `setTimeout()` 을 취소해준다.
    
            → 마우스가 움직이는 동안은 끊임없이 **hide** `setTimeout()` 이 취소될 것이다.
    
        3. 마우스가 멈추면, `clearTimeout()` 은 더이상 **hide** `setTimeout()` 취소 하지 않으므로, 정해진 시간이 지나면, **hide** 가 실행될 것이다.

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -7,7 +7,10 @@ const totalTime = document.getElementById('totalTime');
 const timeline = document.getElementById('timeline');
 const fullScreenBtn = document.getElementById('fullScreen');
 const videoContainer = document.getElementById('videoContainer')
+const videoControls = document.getElementById('videoControls');
 
+let controlsTimeout = null;
+let controlsMovementTimeout = null;
 let volumeValue = .5;
 video.volume = volumeValue;
 
@@ -65,6 +68,26 @@ const handeFullscreen = () => {
     }
 };
 
+const hideControls = () => {
+    videoControls.classList.remove('showing');
+};
+
+const handleMouseMove = () => {
+    if(controlsTimeout){
+        clearTimeout(controlsTimeout);
+        controlsTimeout = null;
+    }
+    if(controlsMovementTimeout){
+        clearTimeout(controlsMovementTimeout);
+        controlsMovementTimeout = null;
+    }
+    videoControls.classList.add('showing');
+    controlsMovementTimeout = setTimeout(hideControls, 3000)
+};
+const handleMouseLeave = () => {
+    controlsTimeout = setTimeout(hideControls, 3000)
+};
+
 playBtn.addEventListener('click', handlePlayClick);
 muteBtn.addEventListener('click', handleMute);
 volumeRange.addEventListener('input', handleVolumeChange);
@@ -72,3 +95,5 @@ video.addEventListener('loadedmetadata', handleLoadedMetadata);
 video.addEventListener('timeupdate', handleTimeUpdate);
 timeline.addEventListener('input', handleTimelineChange)
 fullScreenBtn.addEventListener('click', handeFullscreen);
+video.addEventListener('mousemove', handleMouseMove);
+video.addEventListener('mouseleave', handleMouseLeave);
