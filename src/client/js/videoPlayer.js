bd4863f92830aacf8b755c44e559271d22320685
clean-teach <82921898+clean-teach@users.noreply.github.com>
2022-07-27 03:49:29 +0900

    #11.11 Styles Recap
    - CSS 를 추가하여, 이번 섹션의 기능을 완성하였다.
    - 버튼에 text를 바꿔주는 대신 class를 바뀌줘서 font awesome icon을 바꿨다.
    - control bar에 마우스가 머무르를때에는 control bar를 숨기지 않아야 한다.
    
    ## Challenge
    
    - [ ]  영상 화면 클릭과 키보드의 스페이스 바로 영상의 재생, 일시정지 하는 기능을 구현

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,11 +1,14 @@
 const video = document.querySelector('video');
 const playBtn = document.getElementById('play');
+const playBtnIcon = playBtn.querySelector('i');
 const muteBtn = document.getElementById('mute');
+const muteBtnIcon = muteBtn.querySelector('i');
 const volumeRange = document.getElementById('volume');
 const currentTime = document.getElementById('currentTime');
 const totalTime = document.getElementById('totalTime');
 const timeline = document.getElementById('timeline');
 const fullScreenBtn = document.getElementById('fullScreen');
+const fullScreenBtnIcon = fullScreenBtn.querySelector('i');
 const videoContainer = document.getElementById('videoContainer')
 const videoControls = document.getElementById('videoControls');
 
@@ -20,7 +23,7 @@ const handlePlayClick = (event) => {
     }else{
         video.pause();
     }
-    playBtn.innerText = video.paused ? 'play' : 'Pause';
+    playBtnIcon.classList = video.paused ? 'fas fa-play' : 'fas fa-pause';
 };
 const handleMute = (event) => {
     if(video.muted){
@@ -28,7 +31,7 @@ const handleMute = (event) => {
     }else{
         video.muted = true;
     }
-    muteBtn.innerText = video.muted ? 'Unmute' : 'Mute';
+    muteBtnIcon.classList = video.muted ? 'fas fa-volume-mute' : 'fas fa-volume-up';
     volumeRange.value = video.muted ? 0 : volumeValue;
 };
 const handleVolumeChange = (event) => {
@@ -41,7 +44,7 @@ const handleVolumeChange = (event) => {
 };
 
 const formatTime = (seconds) => {
-	return new Date(seconds * 1000).toISOString().substring(11,19);
+	return new Date(seconds * 1000).toISOString().substring(14,19);
 }
 
 const handleLoadedMetadata = () => {
@@ -61,10 +64,10 @@ const handeFullscreen = () => {
     const fullScreen = document.fullscreenElement;
     if(fullScreen){
         document.exitFullscreen();
-        fullScreenBtn.innerText = 'Enter Full Screen';
+        fullScreenBtnIcon.classList = 'fas fa-expand';
     }else{
         videoContainer.requestFullscreen();
-        fullScreenBtn.innerText = 'Exit Full Screen';
+        fullScreenBtnIcon.classList = 'fas fa-compress';
     }
 };
 
@@ -91,9 +94,9 @@ const handleMouseLeave = () => {
 playBtn.addEventListener('click', handlePlayClick);
 muteBtn.addEventListener('click', handleMute);
 volumeRange.addEventListener('input', handleVolumeChange);
-video.addEventListener('loadedmetadata', handleLoadedMetadata);
+video.addEventListener('loadeddata', handleLoadedMetadata);
 video.addEventListener('timeupdate', handleTimeUpdate);
 timeline.addEventListener('input', handleTimelineChange)
 fullScreenBtn.addEventListener('click', handeFullscreen);
-video.addEventListener('mousemove', handleMouseMove);
-video.addEventListener('mouseleave', handleMouseLeave);
+videoContainer.addEventListener('mousemove', handleMouseMove);
+videoContainer.addEventListener('mouseleave', handleMouseLeave);
