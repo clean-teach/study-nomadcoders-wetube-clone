83b450c67a2b4ceed4b0001d24bf6c6846aaf0fb
clean-teach <82921898+clean-teach@users.noreply.github.com>
2022-07-28 04:07:50 +0900

    #12.0 Register View Controller
    - 영상을 시청 할 때마다 Back-end에 요청을 보내고, 이 영상 id에 해당하는 조회수를 증가시킨다.
    1. Template을 Rendering 하지 않는 View 만들기
    
        → **API View**
    
        - 대부분의 인터넷에서 작동하는 방식
            - 요즘 시대에는 Back-end에서 Template을 Rendering 하지 않는다.
                - `SSR` (Server Side Rendering) : 서버가 템플릿을 렌더링 하는 것.
            - 대부분의 웹 사이트는 Front 와 Back이 나눠져 있다.
                - Back-end
                    - 인증, DB 등을 처리
                    - `NodeJS`
                        - 요즘은 대부분 `NodeJS` 에서 템플릿을 렌더링 하지 않는다.
                - Front-end :
                    - `VanillaJS`, `Svelte`, `ReactJS` 등으로 만듦
        - API :
            - Front-end와 Back-end가 Server를 통하여 통신하는 방식
    2. **API Router** 만들기
        - form을 사용하지 않고 post 요청을 보낸다.
    3. **API Controller** 만들기
        - Rendering 하지 않고 Back-end에 Data만 보낼 것이다.
        - **Video Model**을 찾아서 meat.view 값에 +1 을 누적한다.
        - 그래도 요청에 대한 응답은 하여야 하기 때문에. `return res.status()` 를 응답해준다.
    4. Front-end 에서 URL 요청을 보내본다.
        - Interactivity : URL이 바뀌지 않고도 페이지에 변화가 생기는 것
        - 브라우저가 아니라, Client에서 JavaScript로 요청을 한다.

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ import MongoStore from 'connect-mongo';
 import rootRouter from './routers/rootRouter';
 import userRouter from './routers/userRouter';
 import videoRouter from './routers/videoRouter';
+import apiRouter from './routers/apiRouter';
 import { localsMiddleware } from './middlewares';
 
 const app = express();
@@ -36,7 +37,8 @@ app.use(localsMiddleware);
 app.use('/uploads', express.static('uploads'));
 app.use('/static', express.static('assets'));
 app.use('/', rootRouter);
-app.use('/users', userRouter);
 app.use('/videos', videoRouter);
+app.use('/users', userRouter);
+app.use('/api', apiRouter);
 
 export default app;
