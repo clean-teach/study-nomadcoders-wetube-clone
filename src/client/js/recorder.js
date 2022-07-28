41d0c1a4a2e761d4eaf4ce8fb8e05c4a1eb1039c
clean-teach <82921898+clean-teach@users.noreply.github.com>
2022-07-29 01:54:34 +0900

    #13.2 Recording Video
    1. 기본적으로 init() 함수에서 `navigator.**[MediaDevices.getUserMedia()](https://developer.mozilla.org/ko/docs/Web/API/MediaDevices/getUserMedia)**` method 로 가져온 비디오를 보여주고
    2. Start Button으로 녹화를 시작/정지 한다.
        - **하나의 버튼에 두가지 Event Listener를 사용**하기 위해서 `.addEventListener()` 와 `[.removeEventListener()](https://developer.mozilla.org/ko/docs/Web/API/EventTarget/removeEventListener)` 를 사용한다.
            - `[.removeEventListener()](https://developer.mozilla.org/ko/docs/Web/API/EventTarget/removeEventListener)` : 이벤트 대상에 등록한 수신기를 제거
    
    ## 영상 녹화
    
    1. 녹화 시작 버튼을 누르면 녹화가 시작되고, 10초 후에 녹화를 중지 한다.
    
        [MediaRecorder - Web APIs | MDN](https://developer.mozilla.org/en-US/docs/Web/API/MediaRecorder)
    
        - 미디어를 녹화 해주는 API
        - `[new MediaRecorder(stream)](https://developer.mozilla.org/en-US/docs/Web/API/MediaRecorder/MediaRecorder)`
            - 생성자는 미디어를 인수로 받으며, 미디어를 녹화하는 여러 이벤트와 메서드를 가지고 있다.
            - `[MediaRecorder.start();](https://developer.mozilla.org/en-US/docs/Web/API/MediaRecorder/start_event)`
                - 녹화를 시작하는 메소드
            - `[MediaRecorder.stop();](https://developer.mozilla.org/en-US/docs/Web/API/MediaRecorder/stop_event)`
                - 녹화를 중지하는 메소드
                - `[recorder.ondataavailable()](https://developer.mozilla.org/en-US/docs/Web/API/MediaRecorder/dataavailable_event)` 메소드를 반환
                    - 녹화된 미디어의 정보를 제공

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,13 +1,39 @@
 const startBtn = document.querySelector('#startBtn');
 const previewVideo = document.querySelector('#preview');
 
-const handleStart = async () => {
-    const stream = await navigator.mediaDevices.getUserMedia({
+let stream;
+
+const handleStart = () => {
+    startBtn.innerText = 'Stop Recording';
+    startBtn.removeEventListener('click', handleStart);
+    startBtn.addEventListener('click', handleStop);
+
+    const recorder = new MediaRecorder(stream);
+    recorder.ondataavailable = (e) => {
+        console.log("recording done");
+        console.log(e);
+        console.log(e.data);
+    };
+    console.log(recorder);
+    recorder.start();
+    console.log(recorder);
+    setTimeout(() => {
+        recorder.stop();
+    }, 10000)
+};
+const handleStop = () => {
+    startBtn.innerText = 'Start Recording';
+    startBtn.removeEventListener('click', handleStop);
+    startBtn.addEventListener('click', handleStart);
+};
+const init = async () => {
+    stream = await navigator.mediaDevices.getUserMedia({
         audio: false,
         video: {width: 'auto', height: 'auto'}
     });
     previewVideo.srcObject = stream;
     previewVideo.play();
 };
+init();
 
-startBtn.addEventListener('click', handleStart)
+startBtn.addEventListener('click', handleStart);
